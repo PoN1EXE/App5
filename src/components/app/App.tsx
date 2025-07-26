95bdb679d1aef2f71c47c3da854a5d7477dbe01e
PoN1EXE <PoN1EXE1@gmail.com>
2025-07-26 13:15:26 +0300

    сортировка

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -3,9 +3,16 @@ import { useState } from 'react'
 import { PostList } from '../PostList/PostList'
 import { PostForm } from '../PostForm/PostForm'
 import { PostConst } from '../PostConst/PostConst'
+import { MySelect } from '../UI/select/MySelect'
 
 export const App = () => {
   const [posts, setPosts] = useState(PostConst)
+  const [selectedSort, setSelectedSort] = useState('')
+
+  const sortPosts = (sort) => {
+    setSelectedSort(sort)
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
+  }
 
   const createPostNew = (newPost) => {
     setPosts([...posts, newPost])
@@ -18,6 +25,18 @@ export const App = () => {
   return (
     <div className='app'>
       <PostForm createPost={createPostNew} />
+      <hr style={{ margin: '15px 0' }} />
+      <div>
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue='Сортировка'
+          options={[
+            { value: 'title', name: 'По названию' },
+            { value: 'body', name: 'По описанию' },
+          ]}
+        />
+      </div>
       {posts.length ? (
         <PostList removePost={removePostBtn} posts={posts} title='Список постов' />
       ) : (
